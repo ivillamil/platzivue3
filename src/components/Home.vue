6ae31971224017f35369e2d38d8e394d8db9e90a
tiroldesign <tiroldesign@gmail.com>
2024-06-14 14:43:13 +0000

    SplashScreen component

---
+++ src/components/Home.vue
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<template>
+    <h1>Hola</h1>
+</template>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="splashscreen">
+        <img src="../assets/cashflow-icon.svg" alt="Logo" />
+        <h1>
+            <span>Cash</span>Flow
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+
+.splashscreen {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    flex-direction: column;
+}
+
+h1 {
+    color: var(--brand-blue);
+    font-weight: 400;
+}
+
+h1 span {
+    color: var(--brand-green);
+}
+
+</style>
