178619e345fe821c30320cd45bd0f7a261a5067d
tiroldesign <tiroldesign@gmail.com>
2024-06-14 14:42:52 +0000

    SplashScreen added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import SplashScreen from "./components/SplashScreen.vue";
+import { defineAsyncComponent } from "vue";
+
+export default {
+  components: {
+    SplashScreen,
+    Home: defineAsyncComponent(() => new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(import("./components/Home.vue"))
+      }, 2500)
+    }))
+  }
+}
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <Suspense>
+    <template #default><Home /></template>
+    <template #fallback><SplashScreen /></template>
+  </Suspense>
 </template>
 
 <style scoped>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
 
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
 </style>
